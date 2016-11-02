ed7af217f139290f292b2f07857ce315228964c9
Andy Herzog <andyherzog@Andys-MacBook-Pro.local>
2016-11-01 18:04:26 -0600

    initial commit

--- script.js
+++ script.js
@@ -8,3 +8,17 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+
+  var stopButton = document.getElementById('stopButton');
+  var slowButton = document.getElementById('slowButton');
+  var goButton = document.getElementById('goButton');
+  var bulb = document.getElementsByClassName('bulb');
+  stopButton.addEventListener('click',function() {
+    bulb[0].style.backgroundColor = 'red';
+  });
+  slowButton.addEventListener('click',function() {
+    bulb[1].style.backgroundColor = 'orange';
+  });
+  goButton.addEventListener('click',function() {
+    bulb[2].style.backgroundColor = 'green';
+  });
